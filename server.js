c6c491a94bfec0d84a8c4869a71a48efe6431a2f
kalaay waaye <kalaay1111@gmail.com>
2018-12-10 21:32:21 -0500

    update

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = process.env.PORT || 3000;
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -43,7 +43,7 @@ var characters = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(mystarwars, "view.html"));
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
 app.get("/add", function(req, res) {
@@ -87,7 +87,7 @@ app.post("/api/characters", function(req, res) {
   res.json(newcharacter);
 });
 
-// Starts the server to begin listening.
+// Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
