6b7b84eafbb74381d0e95ecacd8123c78c4d111b
kalaay waaye <kalaay1111@gmail.com>
2018-12-10 21:22:28 -0500

    update

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post("/api/characters", function(req, res) {
   res.json(newcharacter);
 });
 
-// Starts the server to begin listening
+// Starts the server to begin listening.
 // =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
